41be731f8cd8dd25109e6f6d700b9c113ee56077
Nathan Luo <email": null>
2015-12-03 17:40:11 -0500

    Add associations between models including polymorphism for comments and votes

--- db/migrate/20151203220543_create_questions.rb
+++ db/migrate/20151203220543_create_questions.rb
@@ -3,7 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.string  :content, null: false
       t.references :user, null: false
-      t.references :answer, null: false
+      t.references :answer
 
       t.timestamps null: false
     end
