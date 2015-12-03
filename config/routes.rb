cdf35ae8ff5c29bb36a445d3aef3c6bbfdf4ebac
LAURA EVANS <LKSEVANS12@GMAIL.com>
2015-12-03 18:18:43 -0500

    add secure password to model, helper method to application controller, routes, and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
