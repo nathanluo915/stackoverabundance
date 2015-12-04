7c2948387d89d1cc7b88329728c44b7d87a91575
Nathan Luo <email": null>
2015-12-03 21:13:48 -0500

    Add seed for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+u = User.create!(username: "team", email: "team@example.com", password: "123456")
+users = []
+30.times do
+  user = User.create!(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "111111")
+  users << user
+end
+
+puts "Create users"
+
+12.times do
+  print '.'
+  q = Question.create!(content: Faker::Lorem.sentence.sub(/\./, "?"), user: users.sample)
+  Random.rand(3).times do
+    com = Comment.create!(content: Faker::Lorem.sentence, user: users.sample, commentable: q)
+  end
+  10.times do
+    print "+"
+    ans = Answer.create!(content: Faker::Lorem.sentence, user: users.sample, question: q)
+    Random.rand(10).times do
+      com = Comment.create!(content: Faker::Lorem.sentence, user: users.sample, commentable: ans)
+    end
+  end
+  puts
+
+end
