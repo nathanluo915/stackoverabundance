3eccddbffd33852074742a250ce2eedb8cb7fbad
Salim Rahimi <email": null>
2015-12-04 11:38:41 -0500

    mid-commit

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,31 @@ class Question < ActiveRecord::Base
   belongs_to  :best_answer, class_name:  "Answer"
   has_many    :votes, as: :votable
   has_many    :comments, as: :commentable
+
+
+
+def self.get_questions_by_newest
+  order(created_at: :desc).limit(20)
+end
+
+def get_questions_by_
+
+end
+
+def get_votes_count
+   if (self.votes && self.votes.find_by(upvote: false))
+    return (self.votes.count - self.votes.find_by(upvote: false).count)
+  else
+    return 0
+  end
+end
+
+def get_answers_count
+   if (self.answers)
+    return (self.answers.count)
+  else
+    return 0
+  end
+end
+
 end
