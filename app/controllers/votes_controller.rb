7886ce4a28cb2308e78b50e9cd0d36c03147666e
Ian Lockwood <email": null>
2015-12-04 11:12:57 -0500

    Set up votes controller, tests, view partial

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,22 @@
+class VotesController < ApplicationController
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def permit_params
+  end
+end
