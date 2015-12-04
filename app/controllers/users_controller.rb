8bbb4efb21bf925bd3d3d28258d40f8d1177930e
LAURA EVANS <LKSEVANS12@GMAIL.com>
2015-12-03 21:34:52 -0500

    finish new users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(permit_params)
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def permit_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
