57c1d0df1c859748c76cc845d7cb8e8f37e54509
Nathan Luo <email": null>
2015-12-04 10:08:53 -0500

    Change the question answer nested routes and add answer controller with create action

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,17 @@
+class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    answer = @question.answers.build(answer_build_params)
+    if answer.save
+      render partial: answer
+    else
+      @notice = answer.errors.full_messages
+      render question_path(@queston)
+    end
+  end
+
+  private
+  def answer_build_params
+    params.require(:answer).permit(:user_id, :content)
+  end
+end
