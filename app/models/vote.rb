2576eeef8df7be266c4801625ca47642f1e3510c
Ian Lockwood <email": null>
2015-12-04 10:41:51 -0500

    Validate that a user may only vote once per question.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :votable, polymorphic: true
+
+  validates :user, uniqueness: { scope: :votable,
+    message: "You may only vote once per question. If you are seeing this message, you have attempted vote manipulation, which is against our community guidelines. If you believe you are seeing this message by mistake, please contact our team at admin@stackoverabundance.com to avoid banning."}
 end
