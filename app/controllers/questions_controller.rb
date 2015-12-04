2de0ae83384d9444216d0e8233a8b1abacadc611
Salim Rahimi <email": null>
2015-12-03 19:18:48 -0500

    Create new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,4 +5,9 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = current_user.questions.new
+  end
+
 end
