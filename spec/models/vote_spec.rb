b0533ff4099a7e673dc2708199fb85e61cc866db
Ian Lockwood <email": null>
2015-12-05 12:29:37 -0500

    Add initial vote spec framework

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
+  before(:each) do
+    vote1 = Vote.create(user_id: 1, votable:)
+  end
 
+
+  it "should be true by default" do
+
+  end
+
+  it "should have a user id" do
+    pending
+  end
+
+  it "should be associated with a question, answer, or comment" do
+    pending
+  end
+
+  it "One user should not be able to vote twice" do
+    pending
+  end
 end
