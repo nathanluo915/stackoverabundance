c00bf0ff5a375d40b79907d44b0f12120a7fb853
Laura Evans <email": null>
2015-12-05 16:15:55 -0500

    add private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
+  private
+
   def current_user
-    current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
