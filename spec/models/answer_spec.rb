f120d5b63368fa9b48b847b1446108d979e1f56e
Nathan Luo <email": null>
2015-12-05 15:09:49 -0500

    Add association test for answer model and creation test for comment model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,27 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  describe "Answer" do
-    context "Adding record" do
-      it "sucess if all parameters are valid" do
-        answer = FactoryGirl.create(:answer)
-        expect(answer.persisted?).to eq(true)
-      end
+  context "Adding record" do
+    it "sucess if all parameters are valid" do
+      answer = FactoryGirl.create(:answer)
+      expect(answer.persisted?).to eq(true)
+    end
 
-      it "fail if the user does not exist" do
-        answer = Answer.new(content: "HiHi", question: FactoryGirl.create(:question), user_id: 99)
-        expect(answer.valid?).to eq(false)
-      end
+    it "fail if the user does not exist" do
+      answer = Answer.new(content: "HiHi", question: FactoryGirl.create(:question), user_id: 99)
+      expect(answer.valid?).to eq(false)
+    end
 
-      it "fail if the question does not exist" do
-        answer = Answer.new(content: "HiHi", question_id: 1, user: FactoryGirl.create(:user))
-        expect(answer.valid?).to eq(false)
-      end
+    it "fail if the question does not exist" do
+      a = Answer.new(content: "HiHi", user: FactoryGirl.create(:user))
+      expect(a.valid?).to eq false
+    end
 
-      it "fail if the content is nil or empty" do
-        expect{FactoryGirl.create(:answer, content: nil)}.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Content cannot be empty')
-        expect{FactoryGirl.create(:answer, content: "")}.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Content cannot be empty')
+    it "fail if the content is nil or empty" do
+      expect{FactoryGirl.create(:answer, content: nil)}.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Content cannot be empty')
+      expect{FactoryGirl.create(:answer, content: "")}.to raise_error(ActiveRecord::RecordInvalid, 'Validation failed: Content cannot be empty')
+    end
+  end
+
+  context "Association" do
+    it "should return the user who created the answer" do
+      user = FactoryGirl.create(:user)
+      answer = FactoryGirl.create(:answer, user: user)
+      expect(answer.user).to eq(user)
+    end
+
+    it "should return the question it belongs to" do
+      question = FactoryGirl.create(:question)
+      answer = FactoryGirl.create(:answer, question: question)
+      expect(answer.question).to eq(question)
+    end
+
+    it "should return a collection of comments it has" do
+      answer = FactoryGirl.create(:answer)
+      3.times do
+        comments << FactoryGirl.create(:answer_comment, commentable: answer)
       end
+      3.times {FactoryGirl.create(:answer_comment, commentable: answer)}
+
+      expect(answer.comments).to eq(comments)
     end
   end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Adding record" do
+    it "sucess if all parameters are valid" do
+      answer_comment = FactoryGirl.create(:answer_comment)
+      expect(answer_comment.persisted?).to eq(true)
+    end
+
+    it "fail if the user does not exist" do
+      comment = Comment.new(content: "HiHi", commentable: FactoryGirl.create(:answer), user_id: User.last.id + 1)
+      expect(comment.valid?).to eq(false)
+    end
+
+    it "fail if the commentable does not exist" do
+      a = Answer.new
+      comment = Comment.new(content: "HiHi", user: FactoryGirl.create(:user))
+      expect(comment.valid?).to eq(false)
+    end
+
+    it "fail if the content is nil" do
+      c = Comment.new(commentable: FactoryGirl.create(:answer), user: FactoryGirl.create(:user))
+      expect(c.valid?).to eq(false)
+    end
+    it "fail if the content is nil" do
+      c = Comment.new(commentable: FactoryGirl.create(:answer), user: FactoryGirl.create(:user), content: "")
+      expect(c.valid?).to eq(false)
+    end
+  end
+
 end
