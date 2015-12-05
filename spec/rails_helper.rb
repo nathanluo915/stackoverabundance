0492b6bd8e2b5d9655577ea25c86f5733d708127
Laura Evans <email": null>
2015-12-05 15:16:09 -0500

    Add shoulda matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,19 @@ SimpleCov.start
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.include Capybara::DSL
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
