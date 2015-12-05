ba735de38746d81f99fc28b0e1160231fbfd0f23
Ian Lockwood <email": null>
2015-12-05 13:28:53 -0500

    Add finished vote model spec

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,46 +2,26 @@ require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
 
-
-  it "" do
-    pending
+  context "#create" do
+    it "creates a vote with valid params" do
+      Vote
+      pending
+    end
+    it "doesn't create a vote when params are invalid" do
+      pending
+    end
   end
 
+  context "#update" do
+    it "updates a vote with valid params" do
+      pending
+    end
+    it "doesn't update a vote when params are invalid" do
+      pending
+    end
+  end
 
-  # describe "admin panel" do
-  #   it "#index" do
-  #     get :index
-  #     expect(assigns(:posts)).to eq Post.all
-  #   end
-
-  #   it "#new" do
-  #     pending
-  #   end
-
-  #   context "#create" do
-  #     it "creates a post with valid params" do
-  #       pending
-  #     end
-  #     it "doesn't create a post when params are invalid" do
-  #       pending
-  #     end
-  #   end
-
-  #   context "#edit" do
-  #     it "updates a post with valid params" do
-  #       pending
-  #     end
-  #     it "doesn't update a post when params are invalid" do
-  #       pending
-  #     end
-  #   end
-
-  #   it "#destroy" do
-  #     pending
-  #   end
-  # end
-
-
-
-
+  it "#destroy" do
+    pending
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,23 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   before(:each) do
-    vote1 = Vote.create(user_id: 1, votable:)
+    @user = FactoryGirl.create(:user)
+    @question = FactoryGirl.create(:question, user: @user)
+    @vote1 = Vote.create(user: @user, votable: @question)
+    @answer = FactoryGirl.create(:answer, user: @user)
   end
-
-
   it "should be true by default" do
-
+    expect(@vote1.upvote).to eq(true)
   end
 
-  it "should have a user id" do
-    pending
+  it "should have an accurate user id" do
+    expect(@vote1.user_id).to eq(@user.id)
   end
 
-  it "should be associated with a question, answer, or comment" do
-    pending
+  it "should be associated with the correct question, answer, or comment" do
+    expect(@vote1.votable_id).to eq(@question.id)
+    expect(@vote1.votable_type).to eq("Question")
+    answer = FactoryGirl.create(:answer, user: @user)
+    vote2 = Vote.create(user: @user, votable: answer)
+    expect(vote2.votable_id).to eq(answer.id)
+    expect(vote2.votable_type).to eq("Answer")
+    comment = FactoryGirl.create(:comment, commentable: answer, user: @user)
+    vote3 = Vote.create(user: @user, votable: comment)
+    expect(vote3.votable_id).to eq(comment.id)
+    expect(vote3.votable_type).to eq("Comment")
   end
 
   it "One user should not be able to vote twice" do
-    pending
+    bad_vote = Vote.new(user: @user, votable: @question)
+    expect(bad_vote).to_not be_valid
   end
 end
