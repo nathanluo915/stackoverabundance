96f5d5840685e2eaaa545e37089ad3355b0b044e
Nathan Luo <naipton@gmail.com>
2015-12-05 12:53:01 -0500

    Fix factorygirl with polymorphic association models

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,5 +1,20 @@
 FactoryGirl.define do
-  factory :vote do
+  factory :question_vote, class: "Vote" do
+    association :user, factory: :user
+    association :votable, factory: :question
+    upvote  true
+  end
+
+  factory :answer_vote, class: "Vote" do
+    association :user, factory: :user
+    association :votable, factory: :answer
+    upvote  true
+  end
+
+  factory :comment_vote, class: "Vote" do
+    association :user, factory: :user
+    association :votable, factory: :comment
+    upvote  true
   end
 
 end
