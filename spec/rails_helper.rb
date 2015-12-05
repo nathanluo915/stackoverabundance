8d30f25a213cd25b22edf8397ef93db98968dcd5
Laura Evans <email": null>
2015-12-05 14:45:14 -0500

    config for capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'simplecov'
 
@@ -36,6 +37,7 @@ SimpleCov.start
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Capybara::DSL
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
